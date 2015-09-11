6bce62159856be538d0810b5e6d41236539383b1
HJ <HJ@HJ-LAPTOP>
2015-09-11 16:31:40 +0900

    naming changing

--- farmming-tool/src/main/java/com/farmingtool/controller/DictionaryController.java
+++ farmming-tool/src/main/java/com/farmingtool/controller/DictionaryController.java
@@ -1,4 +1,4 @@
-package com.farmmingtool.controller;
+package com.farmingtool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
