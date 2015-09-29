40f6e258cb80ffedcb63d5f76053764ec7b86012
choi <choi@choi-PC>
2015-09-29 16:26:23 +0900

    pull apply push

---
+++ farmming-tool/src/main/java/com/farmingtool/dto/Accident.java
@@ -0,0 +1,27 @@
+package com.farmingtool.dto;
+
+public class Accident {
+	private String content;
+	private String downUrl;
+	private String pClass;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDownUrl() {
+		return downUrl;
+	}
+	public void setDownUrl(String downUrl) {
+		this.downUrl = downUrl;
+	}
+	public String getpClass() {
+		return pClass;
+	}
+	public void setpClass(String pClass) {
+		this.pClass = pClass;
+	}
+	
+}
